5720a70fd012d09fc2bacaaac8dd6c5fee2aab70
mahdis1360 <mahdismoazen@outlook.com>
2020-04-02 20:27:26 -0400

    final exam

---
+++ EFRepository/SchoolContext.cs
@@ -0,0 +1,19 @@
+﻿using Poco;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Text;
+
+namespace EFRepository
+{
+    class SchoolContext : DbContext
+    {
+        public DbSet<StudentPoco> students { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source=LAPTOP-41TMA4UR;Integrated Security=True");
+            base.OnConfiguring(optionsBuilder);
+
+        }
+    }
+}
---
+++ EFRepository/StudentRepo.cs
@@ -0,0 +1,50 @@
+﻿using Poco;
+using Repository;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace EFRepository
+{
+    public class StudentRepo : IDataRepository<StudentPoco>
+
+    {
+        private readonly SchoolContext _context;
+
+        public StudentRepo()
+        {
+            _context = new SchoolContext();
+
+        }
+        public void Add(params StudentPoco[] items)
+        {
+
+            StudentPoco s = new StudentPoco();
+            _context.students.Add(s);
+            _context.SaveChanges();
+        }
+        public IList<StudentPoco> GetAll(params Expression<Func<StudentPoco, object>>[] navigationProperties)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(params StudentPoco[] items)
+        {
+            StudentPoco s = new StudentPoco();
+            StudentPoco student = _context.students.Find(s.Id);
+            if (student != null)
+            {
+                throw new ArgumentOutOfRangeException("i didnot find");
+            }
+            _context.students.Remove(student);
+            _context.SaveChanges();
+        }
+
+        public void Update(params StudentPoco[] items)
+        {
+
+        }
+    }
+}
